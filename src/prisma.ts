ba4cb3ded27c6a1eee716e55818c89f465728d1e
Himanshu2-8 <pragyan.himanshu2004@gmail.com>
2025-08-21 18:27:25 +0530

    changes to prisma

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
+console.log("Railway DATABASE_URL raw:", process.env.DATABASE_URL?.slice(0, 40));
+
+
 const prisma = new PrismaClient();
 
 export default prisma;
